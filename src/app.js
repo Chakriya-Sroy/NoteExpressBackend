14c359bb8f34f7c4d222bebfdcf1a939d2abadef
chakriyasroy <chakriyasroy03@gmail.com>
2025-12-24 14:28:45 +0700

    update: route

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
-import express from 'express';
+import express from "express";
 import dotev from "dotenv";
-import routeAuth from './routes/auth.route.js';
-import routeProfile from './routes/profile.route.js';
+import routeAuth from "./routes/auth.route.js";
+import routeProfile from "./routes/profile.route.js";
 
 dotev.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));    
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/auth',routeAuth);
-app.use('/profile',routeProfile);
+app.use("/", (req, res) => {
+  return res.status(200).send({ message: "Welcome to the CMS API" });
+});
 
-app.use((req,res)=>{
-  return res.status(404).send({error:"Route not found"});
-})
-const port=process.env.PORT || 3004;
+app.use("/health", (req, res) => {
+  return res.status(200).send({ message: "API is healthy" });
+});
+
+app.use("/auth", routeAuth);
+
+app.use("/profile", routeProfile);
+
+app.use((req, res) => {
+  return res.status(404).send({ error: "Route not found" });
+});
+const port = process.env.PORT || 3004;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
